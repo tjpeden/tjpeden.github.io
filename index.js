6434058d8dedc4390379bb9f19b732589e79ca9f
TJ Peden <tj.peden@tjcoding.com>
2016-09-30 00:01:39 -0500

    WIP

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 let Metalsmith  = require('metalsmith');
 let drafts      = require('metalsmith-drafts');
 let layouts     = require('metalsmith-layouts');
+let inPlace     = require('metalsmith-in-place');
+let excerpts    = require('metalsmith-excerpts');
 let redirect    = require('metalsmith-redirect');
 let markdown    = require('metalsmith-markdown');
 let permalinks  = require('metalsmith-permalinks');
@@ -13,16 +15,13 @@ Metalsmith(__dirname)
   title: "Peden Software",
   description: "Projects and Experiments by TJ Peden",
   generator: "Matalsmith",
+  generatorUrl: "http://www.metalsmith.io/",
   url: "http://peden.software/",
 })
 .destination('./public')
 .clean(true)
 .use(drafts())
 .use(markdown())
-.use(layouts({
-  engine: 'handlebars',
-  default: 'default.html',
-}))
 .use(permalinks({
   pattern: ':title',
   linksets: [
@@ -32,7 +31,15 @@ Metalsmith(__dirname)
     },
   ],
 }))
+.use(excerpts())
 .use(collections())
+.use(inPlace({
+  engine: 'handlebars',
+}))
+.use(layouts({
+  engine: 'handlebars',
+  default: 'default.html',
+}))
 .use(redirect({
   '/2016/09/13/on-the-fence.html': '/on-the-fence',
 }))
